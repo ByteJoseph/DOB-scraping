dec36a3d1de45fe3afc851d83e2a8d133417cb2f
Joseph <josephnjoseph43@gmail.com>
2024-05-20 08:45:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if regno:
             status.update(label="DOB Found!", state="complete", expanded=False)
             st.success(f"DOB Found: {date_str}/{month_str}/{year_str}")
             # HTML code to create a clickable link that opens in a new tab
-            link = f'[View Score](https://results.kite.kerala.gov.in/hse/result_schemeI.html?regno={regno}&date1={date_str}%2F{month_str}%2F{year_str}&Submit=Submit)'
+            link = f'[View Mark sheet](https://results.kite.kerala.gov.in/hse/result_schemeI.html?regno={regno}&date1={date_str}%2F{month_str}%2F{year_str}&Submit=Submit)'
             st.markdown(link, unsafe_allow_html=True)
     else:
             status.update(label="Brute force attack unsuccessful.", state="complete", expanded=False)
